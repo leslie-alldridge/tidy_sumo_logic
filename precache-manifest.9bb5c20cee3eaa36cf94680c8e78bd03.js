58e6c282967fd2f2b77a421bce1834126d18ce1c
leslie-alldridge <leslie.alldridge@gmail.com>
2020-06-04 13:34:12 +1200

    Updates

---
+++ precache-manifest.9bb5c20cee3eaa36cf94680c8e78bd03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f5b918e401c1ee217b3b4703b6574c",
+    "url": "/tidy_sumo_logic/index.html"
+  },
+  {
+    "revision": "e5cacf6ad1045a08f4fa",
+    "url": "/tidy_sumo_logic/static/css/main.1bbec266.chunk.css"
+  },
+  {
+    "revision": "19d3a512f31f20ff7dd5",
+    "url": "/tidy_sumo_logic/static/js/2.5c1118cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tidy_sumo_logic/static/js/2.5c1118cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5cacf6ad1045a08f4fa",
+    "url": "/tidy_sumo_logic/static/js/main.539b1bb9.chunk.js"
+  },
+  {
+    "revision": "b3baa37cafed84364d43",
+    "url": "/tidy_sumo_logic/static/js/runtime-main.dac845e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tidy_sumo_logic/static/media/logo.5d5d9eef.svg"
+  }
+]);
